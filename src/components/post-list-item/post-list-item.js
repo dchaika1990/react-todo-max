12adda04ca36d10bb97cbc30d379e96b09e1f3d6
dchaika1990 <dchaika@inbox.ru>
2020-12-23 23:36:13 +0200

    add props to post-item

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './post-list-item.css'
 
-const PostListItem = () => {
+const PostListItem = ({label, important = false}) => {
+	let classNames = `app-list-item d-flex justify-content-between`;
+	classNames += important ? ' important' : '';
 	return (
-		<li className={`app-list-item d-flex justify-content-between`}>
+		<div className={classNames}>
 			<span className={`app-list-item-label`}>
-				hello !!!!
+				{label}
 			</span>
 			<div className={`d-flex justify-content-center align-items-center`}>
 				<button
@@ -22,7 +24,7 @@ const PostListItem = () => {
 				</button>
 				<i className={`fa fa-heart`}/>
 			</div>
-		</li>
+		</div>
 	)
 }
 
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,12 +3,23 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css'
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+	const elements = posts.map(post => {
+		const {id, ...itemProps} = post;
+		return (
+			<li key={id} className={`list-group-item`}>
+				<PostListItem {...itemProps}
+					// label={post.label}
+					// important={post.important}
+				/>
+			</li>
+		)
+	})
+
 	return (
 		<ul className={`app-list list-group`}>
-			<PostListItem />
-			<PostListItem />
-			<PostListItem />
+			{elements}
 		</ul>
 	)
 }
